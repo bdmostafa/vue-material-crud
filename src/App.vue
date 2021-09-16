9aead0af4f36fbbd0682522f3f1dca2d435e4f7d
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-16 12:23:31 +0600

    project init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <md-button class="md-raised md-primary">Primary</md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
